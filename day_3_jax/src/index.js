42d6f1017d9eeff7f2ca00667d357d14d76c9dce
xyq <2466257007@qq.com>
2018-10-29 17:23:50 +0800

    commit JAX

--- day_3_jax/src/index.js
+++ day_3_jax/src/index.js
@@ -1,12 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import logo from './logo.svg';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//传入变量
+class Variable extends React.Component{
+    render(){
+        const name = 'Variable';
+            return (
+                <h2 className='variable'><label>{name} is FUN</label></h2>
+        );
+    }
+}
+
+//传入函数
+class Function extends React.Component{
+
+    formatName(user){
+        return 'Function ' +user.firstName + "\n" + user.lastName;
+    }
+
+    render(){
+        const user = {
+            firstName: 'Shylot',
+            lastName: 'Xu'
+        }
+
+        return (
+            <span className='function'>
+                {this.formatName(user)}
+            </span>
+        );
+    }
+}
+
+class Attribute extends React.Component{
+    render(){
+        return (
+            <div className='attribute'>
+                <img src={logo}></img>
+
+                <div>
+                    <a href='www.baidu.com' tabIndex='0'>百度</a><br/>
+                    <a href='www.google.com' tabIndex='1'>谷歌</a><br/>
+                    <a href='www.github.com' tabIndex='2'>谷歌</a><br/>
+                </div>
+            </div>
+        );
+    }
+}
+
+class Container extends React.Component{
+
+    render(){
+        return (
+            <div>
+                <Variable/>
+                <Function/>
+                <Attribute/>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+
+    <Container />,
+    document.getElementById('root')
+);
