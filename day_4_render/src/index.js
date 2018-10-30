a78ff648148754892a65cac2b0317d38834e2d92
xyq <2466257007@qq.com>
2018-10-30 09:37:17 +0800

    add render

--- day_4_render/src/index.js
+++ day_4_render/src/index.js
@@ -1,3 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+function timer(){
+    const element = (
+        <div>
+            <h1>Hello World, Hello Erya！</h1>
+            <h2>Time: {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    )
+    ReactDOM.render(element,document.getElementById('root'));
+}
+
+setInterval(timer,1000);
